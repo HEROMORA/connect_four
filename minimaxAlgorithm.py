f48a22e45fc5e97ea7bc7f4bbd4fd717f0fc4f1e
Amr <amrbreekaa@gmail.com>
2022-04-09 23:44:38 +0200

    number of explored nodes

--- minimaxAlgorithm.py
+++ minimaxAlgorithm.py
@@ -8,8 +8,11 @@ import time
 # Red --> Model (MAXIMIZER)
 # Blue --> User (MINIMIZER)
 
+exploredNodes = 0
+
 
 def maximize(board_state: State, k: int, prune: bool, tree, alpha: int, beta: int):  # boardState is state object, k is number of levels
+    global exploredNodes
     if not k or board_state.is_full_board():
         score = board_state.evaluate_set_cost()  # End of tree, get score using heuristics
         return None, score
@@ -19,11 +22,12 @@ def maximize(board_state: State, k: int, prune: bool, tree, alpha: int, beta: in
 
     board_state.generate_children()
     for child in board_state.children:
-
-        n = tree.create_node(f'{child.node_type}:{0}', child.id, parent=board_state.id)
+        exploredNodes += 1
+        n = tree.create_node(f'{child.node_type.name}:{0}', child.id, parent=board_state.id)
         _, utility = minimize(child, k - 1, prune, tree, alpha, beta)
 
-        n.tag = f'{child.node_type}:{utility}'
+        n.tag = f'{child.node_type.name}:{utility}'
+
         if maxChild is None or utility > max_utility:
             maxChild = child
             max_utility = utility
@@ -39,6 +43,7 @@ def maximize(board_state: State, k: int, prune: bool, tree, alpha: int, beta: in
 
 
 def minimize(board_state: State, k: int, prune: bool, tree, alpha, beta):
+    global exploredNodes
     if not k or board_state.is_full_board():
         score = board_state.evaluate_set_cost()  # End of tree, get score using heuristics
         return None, score
@@ -48,11 +53,12 @@ def minimize(board_state: State, k: int, prune: bool, tree, alpha, beta):
     board_state.generate_children()
 
     for child in board_state.children:
-
-        n = tree.create_node(f'{child.node_type}:{0}', child.id, parent=board_state.id)
+        exploredNodes += 1
+        n = tree.create_node(f'{child.node_type.name}:{0}', child.id, parent=board_state.id)
         _, utility = maximize(child, k - 1, prune, tree, alpha, beta)
 
-        n.tag = f'{child.node_type}:{utility}'
+        n.tag = f'{child.node_type.name}:{utility}'
+
         if minChild is None or utility < min_utility:
             minChild = child
             min_utility = utility
@@ -68,16 +74,20 @@ def minimize(board_state: State, k: int, prune: bool, tree, alpha, beta):
 
 
 def decide(board_state: State, k: int, prune: bool, color: str):
+    global exploredNodes
     node = None
     tree = Tree()
-    root = tree.create_node(f'{board_state.node_type}:{0}', board_state.id)
+    root = tree.create_node(f'{board_state.node_type.name}:{0}', board_state.id)
     time1 = time.time()
 
     pruning = Pruning(-float('inf'), float('inf'))
     node, utility = maximize(board_state, k, prune, tree, pruning.alpha, pruning.beta)
 
-    root.tag = f'{board_state.node_type}:{utility}'
+    root.tag = f'{board_state.node_type.name}:{utility}'
     totalTime = time.time() - time1
     tree.show()
-    print(totalTime)
+    print(f'Execution time = {totalTime}')
+    print(f'Number of Explored Nodes = {exploredNodes}')
+    print('------------------------------------------------------')
+    exploredNodes = 0
     return node
